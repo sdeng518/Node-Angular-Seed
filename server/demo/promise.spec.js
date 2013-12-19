b47614a2c268b0a5d910ef898936737eddbf1281
BiDanJun <11084184@qq.com>
2013-12-19 08:56:55 +0800

    user login callback version

--- server/demo/promise.spec.js
+++ server/demo/promise.spec.js
@@ -15,7 +15,7 @@ function cbLogin(user,password,cb)
             cb("user"+user+" not exist",false)
             return
         }
-        if (users[user].password!=password)
+        if (users[user].password!==password)
         {
             cb("password invalid",false);
             return
@@ -34,10 +34,24 @@ describe("promise demo", function () {
         })
     })
 
+    //demo test
+    it("test callback login", function (done) {
+        cbLogin('张三','2',function(err,data)
+        {
+            expect(data).toEqual(false)
+            expect(err).toEqual('password invalid')
+            done();
+        })
+    })
+
     it("test callback login should fail", function (done) {
         cbLogin('王麻子','1',function(err,data)
         {
-            expect(data).toEqual(true)
+            //键值对的键不存在，为undefine
+            if (!users['王麻子'])
+                console.log('undefine类型为false')
+            expect(data).toEqual(false);
+            expect(err).toEqual('user王麻子 not exist');
             done();
         })
     })
